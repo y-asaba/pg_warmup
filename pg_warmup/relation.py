e83cd71b6711dacb6924e249382341d6eee7493e
Yoshiyuki Asaba <yoshiyuki_asaba@spotlig.ht>
2014-02-12 00:33:10 +0900

    Register PyPI

--- pg_warmup/relation.py
+++ pg_warmup/relation.py
@@ -1,18 +1,15 @@
-#! /usr/bin/env python
 #/* -*- coding: utf-8 -*- */
 
 import sys
 import psycopg2
-from os import devnull
-from subprocess import check_output, call
-from optparse import OptionParser
+from subprocess import check_output
 
 
 class RelationNotFoundException(Exception):
     pass
 
 
-class Relation:
+class Relation(object):
     def __init__(self, dbname, relname, size, path):
         self.dbname = dbname
         self.relname = relname
@@ -105,58 +102,8 @@ SELECT relname, current_setting('data_directory') || '/' ||  pg_relation_filepat
                 print("")
 
 
-class Index():
+class Index(object):
     def __init__(self, index_name, path, size):
         self.index_name = index_name
         self.size = int(size)
         self.path = path
-
-
-if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option('-t', '--table', dest='table', type="string", help="warmup the named table")
-    parser.add_option('-d', '--database', dest='database', type="string", help="dbname")
-    parser.add_option('-i', action='store_true', dest='use_ionice',
-                      default=False, help="use ionice command")
-    parser.add_option('-s', action='store_true', dest='use_shared_buffer',
-                      default=False, help="cache on shared buffer, not page cache")
-    parser.add_option('-x', action='store_false', dest='dryrun',
-                      default=True, help="execute warmup")
-
-    (options, args) = parser.parse_args()
-    try:
-        if options.database is None:
-            print("Please specify a database name using -d option")
-            sys.exit(1)
-
-        if options.table is None:
-            print("Please specify a table name using -t option")
-            sys.exit(1)
-
-        if options.use_ionice:
-            #
-            # Check if ionice is installed
-            #
-            with open(devnull, "w") as fnull:
-                try:
-                    ret = call(['ionice', '-h'], stdout=fnull, stderr=fnull)
-                    if ret != 0:
-                        print("ionice is not installed. Please install ionice or run pg_warmup without -i option")
-                        sys.exit(1)
-                except Exception as ex:
-                    #
-                    # In python 3.3, FileNotFoundException will be occured.
-                    # In python 2.7, OSError will be occured.
-                    #
-                    print("ionice is not installed. Please install ionice or run pg_warmup without -i option")
-                    sys.exit(1)
-
-        relation = Relation.get_relation(options.database, options.table)
-        relation.print_relation()
-        relation.warmup(use_shared_buffer=options.use_shared_buffer, use_ionice=options.use_ionice, dryrun=options.dryrun)
-        if options.dryrun:
-            print("\033[31m!! This is a dry run. Please specify -x option to warmup !!\033[0m")
-
-    except RelationNotFoundException:
-        print("ERROR: \"%s\" table does not exist" % options.table)
-        sys.exit(1)
