8820a9761e43b731f413de8cbaf55654d9df2acd
Yoshiyuki Asaba <yoshiyuki_asaba@spotlig.ht>
2014-02-14 09:01:49 +0900

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 requires = ['psycopg2==2.5.1']
 
 setup(name='pg_warmup',
-      version          = '0.1.0',
+      version          = '0.2.0',
       description      = 'PostgreSQL warmup tool',
       author           = 'Yoshiyuki Asaba',
       author_email     = 'ysyk.asaba@gmail.com',
